1ca7ccb49a7e3cc32a97861e5af5c661b75ad395
Aaryan Raj <87359124+BlackDracoYT@users.noreply.github.com>
2025-08-29 21:56:24 -0700

    Add files via upload

---
+++ c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int a,b;
+    printf("ENTER the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value b:");
+    scanf("%d",&b);
+    printf("the sum of a and b is;%d", a+b );
+    return 0;
+}
